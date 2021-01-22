c1d9532b39838023a06166b428d7962e2e2117cb
panna001 <sonar.pocket.0926@gmail.com>
2021-01-22 07:42:33 +0000

    [Add]notice

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @user = User.find(current_user.id)
       @books = Book.all
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render :edit
     end
