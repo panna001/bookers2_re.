9c458d7fdad194733b2a608473af491d9384a2ce
panna001 <sonar.pocket.0926@gmail.com>
2021-01-22 09:58:48 +0000

    [Add]編集ページの閲覧権限

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a3b8f2435dbbd0698ac8ddad44db6fb35a7eb7dbb3a01c960d98239bf2692a7c77ae0bd565f784ad150b1771d75eb0fa0f7b51e5aee57f496b14d7e18932b6d7'
