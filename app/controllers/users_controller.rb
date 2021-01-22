cf31253fd79a9218f81ea576aa6e790f80bc401f
panna001 <sonar.pocket.0926@gmail.com>
2021-01-22 04:07:45 +0000

    [Add]header&user_show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get "/about" => "home#about"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :index, :show, :edit, :update, :destroy]
 end
